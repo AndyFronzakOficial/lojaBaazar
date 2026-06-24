37ecc5e87c0755ca1a835396c775979a249e94a2
Anderson Nunes Perugorria <30420866+AndyFronzakOficial@users.noreply.github.com>
2026-06-23 21:59:15 -0300

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
-  supabaseUrl || 'https://example.supabase.co',
-  supabaseAnonKey || 'example-key'
+  supabaseUrl,
+  supabaseAnonKey
 )
